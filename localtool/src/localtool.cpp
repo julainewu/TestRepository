159baeae24d2195dd00954836da36aa593fa2612
julainewu <julaine.wu@gmail.com>
2022-04-17 18:24:08 -0400

    Added new function in the main

--- localtool/src/localtool.cpp
+++ localtool/src/localtool.cpp
@@ -14,15 +14,38 @@ using namespace std;
 int Calculator (int num1, int num2, int option);
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "This App shows Data" << endl;
-	cout << "Test the process of github with eclipse" << endl;
 
-	int option;
+	/*int option;
 	option = 1;
 
 	int num1 =10, num2 = 5;
 
-	cout << Calculator (num1, num2, option);
+	cout << "The addition result is = " << Calculator (num1, num2, option);*/
+
+	char setOperator;
+	float firstNum = 0.00, secondNum = 0.00;
+	cout << "Enter your operator: +, -, *, /: \n";
+	cin >> setOperator;
+	cout <<"Enter your first calculation:  \n";
+	cin >> firstNum;
+	cout <<" Enter your second calculation:  \n";
+	cin >> secondNum;
+
+	switch (setOperator) {
+	case '+':
+		cout << "The answer is: " << firstNum + secondNum;
+		break;
+	case '-':
+		cout << "The answer is: " << firstNum - secondNum;
+		break;
+	case '*':
+		cout << "The answer is: " << firstNum * secondNum;
+		break;
+	case '/':
+		cout << "The answer is: " << firstNum / secondNum;
+		break;
+
+	}
+
 	return 0;
 }
