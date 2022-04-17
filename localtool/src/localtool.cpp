7ca42bd74058dcc92d6c422cb20e1be2566e19e9
julainewu <julaine.wu@gmail.com>
2022-04-17 14:46:29 -0400

    Inital commit of project

---
+++ localtool/src/localtool.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : localtool.cpp
+// Author      : Julaine
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "This App shows Data" << endl;
+	return 0;
+}
